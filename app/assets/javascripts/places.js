d4ede2408c4dbc32d51d069dbf987c9ebd71f770
jason <jason.addie@gmail.com>
2013-12-20 15:32:01 +0400

    changed eval form so only show one category at a time

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -3,59 +3,21 @@ $(document).ready(function(){
   /* venue filter on place form */
   if (gon.place_form_venue_filter){
     $('ul#venue_categories li a').click(function(){
+      // unmark all links as active
+      $('ul#venue_categories li a').removeClass('active');
+      // mark this link as active
+      $(this).addClass('active');
+
       // turn off all lists
       $('#venue_category_lists .venue_category').addClass('accessibly-hidden');
-      
       // turn on the correct list
       $('#venue_category_lists .venue_category[data-id="' + $(this).data('id') + '"]').removeClass('accessibly-hidden').focus();
-      
+     
+      // make sure window is at the top
+      $(window).scrollTop(0);
     });
-
-/*
-    // custom css expression for a case-insensitive contains()
-    jQuery.expr[':'].Contains = function(a,i,m){
-      return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
-    };
-
-
-    $('#filter-input').change( function () {
-      var filter = $(this).val();
-      var list = '.venue-category ul';
-      if(filter) {
-        // this finds all links in a list that contain the input,
-        // and hide the ones not containing the input while showing the ones that do
-        $(list).find("a:not(:Contains(" + filter + "))").parent().slideUp().addClass('hidden').removeClass('visible');
-        $(list).find("a:Contains(" + filter + ")").parent().slideDown().addClass('visible').removeClass('hidden');
-        
-      } else {
-        $(list).find("li").slideDown().addClass('visible').removeClass('hidden');
-      }
-
-      // show number of matches
-      $('.venue-filter-num-match').html($(list).find('li.visible').length + ' ' + gon.place_form_venue_num_match);
-
-      // if no matches, hide everything
-      // else, only show sections that have match in list
-      if ($(list).find('li.visible').length == 0){
-        $('.venue-category').slideUp();
-      } else {
-        // only category if have results
-        $('.venue-category').each(function(){
-          if ($(this).find('li.visible').length == 0){
-            $(this).slideUp();
-          }else{
-            $(this).slideDown();
-          }
-        });
-      }
-      return false;
-    })
-    .keyup( function () {
-      // fire the above change event after every letter
-      $(this).change();
-    });
-*/
   }
+  
   
   /* show name for place form */
   if (gon.show_place_name_form){
@@ -125,15 +87,42 @@ $(document).ready(function(){
         $(ths).closest('div.venue_evaluation_question').find('input[type="text"]').attr('aria-hidden', 'true');
       }
     }
-  
+
+    // show the submit button if at least one item is selected
+    $('form.place div.venue_evaluation input[type="radio"]').change(function(){
+      if ($('form.place div.venue_evaluation input[type="radio"]:checked').length == 0){
+        $('form.place #submit-button').addClass('accessibly-hidden');
+      }else{
+        $('form.place #submit-button').removeClass('accessibly-hidden');
+      }
+    });
+
+    // if radio button with evidence is selected, show the text box to get the evidence
     $('form.place div.venue_evaluation div.question-with-evidence input[type="radio"]').change(function(){
       show_question_evidence(this);
     });
 
-    /* if any vields are already marked, show evidence if necessary */  
+    // if any fields are already marked, show evidence if necessary 
     $('form.place div.venue_evaluation div.question-with-evidence input[type="radio"]:checked').each(function(){
       show_question_evidence(this);
     });
+    
+    // when click on question category navigation, show the appropriate section
+    $('ul.question_categories li a').click(function(){
+      // unmark all links as active
+      $('ul.question_categories li a').removeClass('active');
+      // mark this link as active
+      $('ul.question_categories li a[data-id="' + $(this).data('id') + '"]').addClass('active');
+
+      // turn off all questions
+      $('#question_category_lists .question_category').addClass('accessibly-hidden');
+      // turn on the correct questions
+      $('#question_category_lists .question_category[data-id="' + $(this).data('id') + '"]').removeClass('accessibly-hidden').focus();
+      
+      // make sure window is at the top
+      $(window).scrollTop(0);
+    });
+    
   }
 
 
