52d319c752f4534576e3986e7d2bfa5b49e3d6a1
jason <jason.addie@gmail.com>
2014-02-18 14:21:21 +0400

    added geolocation when adding a place

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,4 +1,39 @@
 $(document).ready(function(){
+  // get the user coordinates
+  function get_user_coordinates(position){
+    load_place_form_map([position.coords.latitude, position.coords.longitude]);
+  }
+
+  function get_geolocation_error(error) {
+    var errors = { 
+      1: 'Permission denied',
+      2: 'Position unavailable',
+      3: 'Request timeout'
+    };
+    console.log("geo location error: " + errors[error.code]);
+    load_place_form_map();
+  }
+
+  function load_place_form_map(coords){
+    console.log("- coords " + coords);
+    if (coords == undefined){
+      console.log("- coords not passed in, using default gon coords");
+      coords = [gon.lat, gon.lon];
+    }
+    
+    map = L.map(gon.map_id).setView(coords, gon.zoom);
+    L.tileLayer(gon.tile_url, {maxZoom: gon.max_zoom}).addTo(map);
+	  map.attributionControl = false;
+	  map.zoomControl = true;
+	
+    function onMapClick(e){
+      reset_map();
+      create_map_marker(e.latlng.lat, e.latlng.lng, null, true);
+    }
+    
+    map.on('click', onMapClick);  
+  }
+
   /*************************************************/
   /* venue filter on place form */
   if (gon.place_form_venue_filter){
@@ -49,17 +84,15 @@ $(document).ready(function(){
   
   /* show map for place form */
   if (gon.show_place_form_map){
-      map = L.map(gon.map_id).setView([gon.lat, gon.lon], gon.zoom);
-      L.tileLayer(gon.tile_url, {maxZoom: gon.max_zoom}).addTo(map);
-		  map.attributionControl = false;
-		  map.zoomControl = true;
-		
-      function onMapClick(e){
-        reset_map();
-        create_map_marker(e.latlng.lat, e.latlng.lng, null, true);
-      }
-      
-      map.on('click', onMapClick);  
+      // if the user is using a browser that supports geolocation
+      // use the current users coordinates
+      if (navigator.geolocation){
+        console.log('user can use geolocation');
+        // not catching any errors for if there are some, the var coords will not change and that is ok
+        navigator.geolocation.getCurrentPosition(get_user_coordinates, get_geolocation_error);
+      } else{
+        load_place_form_map();
+      }  
   }
   
   /* perform an address search */
