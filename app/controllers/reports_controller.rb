e4c055af23fc8bbb3586d925a063ebe390d13590
jason <jason.addie@gmail.com>
2014-02-27 15:38:13 +0400

    added general stats

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,6 +4,13 @@ class ReportsController < ApplicationController
     @places = PlaceSummary.stats_overall_place_evaluation_results
     @evals = PlaceSummary.stats_overall_evaluation_results
     @disabilities = Disability.is_active.sorted
+    @general_stats = []
+    @general_stats << {:name => I18n.t('reports.index.general.places'), :count => Place.count}
+    @general_stats << {:name => I18n.t('reports.index.general.certified'), :count => @evals.present? ? @evals[:certified][:total][:total] : 0}
+    @general_stats << {:name => I18n.t('reports.index.general.public'), :count => @evals.present? ? @evals[:public][:total][:total] : 0}
+    @general_stats << {:name => I18n.t('reports.index.general.place_images'), :count => PlaceImage.count}
+    @general_stats << {:name => I18n.t('reports.index.general.users'), :count => PlaceEvaluation.user_count}
+    
 
     respond_to do |format|
       format.html 
