ebb4ef1e6acb935ae2bdb4e9dfdd059bda7d15be
jason <jason.addie@gmail.com>
2014-07-24 12:15:01 +0400

    fix production deploy paths

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,8 @@
 server "ability.ge", :web, :app, :db, primary: true # server where app is located
 set :application, "Ability" # unique name of application
 set :user, "ability" # name of user on server
-set :ngnix_conf_file_loc, "staging/nginx.conf" # location of nginx conf file
-set :unicorn_init_file_loc, "staging/unicorn_init.sh" # location of unicor init shell file
+set :ngnix_conf_file_loc, "production/nginx.conf" # location of nginx conf file
+set :unicorn_init_file_loc, "production/unicorn_init.sh" # location of unicor init shell file
 set :github_account_name, "JumpStartGeorgia" # name of accout on git hub
 set :github_repo_name, "Maps-for-Advocacy" # name of git hub repo
 set :git_branch_name, "master" # name of branch to deploy
